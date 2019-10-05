6118010c9daf4b8a580e63f8252138cd6fb40dfb
Timotej Fartek <fartek@outlook.com>
2019-10-05 18:32:42 +0200

    Board: Implement feature for showing question details (#22)

--- lib/expert_advice/board/board.ex
+++ lib/expert_advice/board/board.ex
@@ -13,4 +13,18 @@ defmodule ExpertAdvice.Board do
     |> BoardStorage.list_root_posts()
     |> PostParser.parse_from_posts(Question)
   end
+
+  @spec show_details(binary) :: Question.t()
+  def show_details(slug) do
+    slug
+    |> BoardStorage.get_post_with_subposts_by_slug()
+    |> post_to_question_with_answers()
+  end
+
+  @spec post_to_question_with_answers(BoardStorage.Post.t() | nil) :: Question.t() | nil
+  defp post_to_question_with_answers(nil), do: nil
+
+  defp post_to_question_with_answers(post) do
+    PostParser.parse_from_post(post, Question, load_answers: true)
+  end
 end
