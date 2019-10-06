7899971245945f9e6b73f9c18d46bc7735014596
Timotej Fartek <fartek@outlook.com>
2019-10-06 13:50:49 +0200

    Phoenix: Initial setup

---
+++ lib/expert_advice_web/controllers/auth_controller.ex
@@ -0,0 +1,19 @@
+defmodule ExpertAdviceWeb.AuthController do
+  use ExpertAdviceWeb, :controller
+
+  def new(conn, _params) do
+    :todo
+  end
+
+  def create(conn, _params) do
+    :todo
+  end
+
+  def show(conn, _params) do
+    :todo
+  end
+
+  def authenticate(conn, _params) do
+    :todo
+  end
+end
