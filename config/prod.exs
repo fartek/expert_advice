86923f4332fcc2a0771a34a2f147253477096305
Timotej Fartek <fartek@outlook.com>
2019-10-07 00:17:18 +0200

    Config: Deployment settings for heroku (#56)

--- config/prod.exs
+++ config/prod.exs
@@ -11,11 +11,18 @@ use Mix.Config
 # before starting your production server.
 config :expert_advice, ExpertAdviceWeb.Endpoint,
   http: [:inet6, port: System.get_env("PORT") || 4000],
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  url: [scheme: "https", host: "shielded-crag-26913", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
+
+config :expert_advice, ExpertAdviceStorage.Repo,
+  ssl: true,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
 # ## SSL Support
 #
@@ -68,4 +75,4 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs which should be versioned
 # separately.
-import_config "prod.secret.exs"
+# import_config "prod.secret.exs"
