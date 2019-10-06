48095942cc787c2e0c913e31d3078cda650b61dc
Timotej Fartek <fartek@outlook.com>
2019-10-06 17:35:56 +0200

    Mix: Add phoenix_ecto dependency for using schemas to validate user input

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule ExpertAdvice.MixProject do
   def application do
     [
       mod: {ExpertAdvice.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :phoenix_ecto]
     ]
   end
 
@@ -36,6 +36,7 @@ defmodule ExpertAdvice.MixProject do
       {:phoenix_pubsub, "~> 1.1"},
       {:phoenix_html, "~> 2.11"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
+      {:phoenix_ecto, "~> 4.0"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
